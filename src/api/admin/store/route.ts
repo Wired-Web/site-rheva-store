9d8948b4ccf83bc65a59cf7b18adf3d9d6e06262
Julian Blair <j-lee-blair@users.noreply.github.com>
2025-07-21 17:07:03 +0200

    added cors headers custom overrides for OPTIONS

--- src/api/admin/store/route.ts
+++ src/api/admin/store/route.ts
@@ -1,16 +1,9 @@
 import type { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
 
-export async function GET(
-  req: MedusaRequest,
-  res: MedusaResponse
-) {
-  res.sendStatus(200);
-}
-
 export async function OPTIONS(req: MedusaRequest, res: MedusaResponse) {
   res.setHeader("Access-Control-Allow-Origin", "https://site-rheva-admin-guwc.vercel.app");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   res.status(204).end();
-}
+} 
